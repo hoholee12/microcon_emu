de44454e579fedb0755df2e24cb0311b1d0cad31
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2025-02-02 15:24:14 +0900

    more additions to CPU

--- armv7m_emu/CPU.cpp
+++ armv7m_emu/CPU.cpp
@@ -9,6 +9,14 @@ void CPU_init(uint32 pc_pos, uint32 sp_pos) {
 	// set start pc
 	CPU_write_PC(pc_pos);
 	CPU_write_SP(sp_pos);
+
+	// clear all apsr flags
+	CPU_write_apsr_n(0);
+	CPU_write_apsr_z(0);
+	CPU_write_apsr_c(0);
+	CPU_write_apsr_v(0);
+
+
 }
 
 // execute one cycle
--- armv7m_emu/CPU.hpp
+++ armv7m_emu/CPU.hpp
@@ -12,17 +12,40 @@
 // cpu register
 /*
 *	r0~r15 (r13: SP, r14: LR, r15: PC)
-* 
+*	apsr (n: negative(1 if result is negative number), z: zero(1 if zero, 0 if not), c: carry(unsigned overflow),
+*   v: overflow (signed overflow - probably bad), q: for DSP instructions- not supported)
 * 
 */
 struct CPU_struct_reg {
 	uint32 r[16];	// r13: SP, r14: LR, r15: PC
+	uint32 apsr;	// n(31), z(30), c(29), v(28), q(27), 0, 0, 0, 0, 0, 0, 0, ge2, ge1, ge0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+					// <ge is for dsp extension only>
+	uint32 fpscr;	// for floating point control status reg. n, z, c, v is in same bit index as apsr.
+
+	uint32 exception;	// exception number. should be 0 on running.
+	uint32 primask, faultmask, basepri[16];
+
+	uint32 ccr;	// control register
+				// BP(18): enable branch prediction
+				// IC(17): enable icache
+				// DC(16): enable dcache
+				// STKALIGN(9): 0 - 4byte sp align, 1 - 8byte sp align <on exception entry>
+				// BFHFNMIGN(8): 0 - fault causes lockup, 1 - fault ignored
+				// DIV_0_TRP(4): 0 - divby0 dont trap, 1 - traps
+				// UNALIGN_TRP(3): 0 - unaligned access dont trap, 1 - traps
+				// USERSETMPEND(1): 0 - unprivileged cannot access STIR(software trigger interrupt reg), 1 - can
+				// NONBASETHRDENA(0): 0 - attempting to return to thread mode WHILE there are remaining active exceptions will trigger another exception, 1 - can return fine because assume its controlled.
 };
+
 extern struct CPU_struct_reg CPU_var_reg;
 
 #define CPU_write_SP(x) do{CPU_var_reg.r[13] = x; }while(0);
 #define CPU_write_LR(x) do{CPU_var_reg.r[14] = x; }while(0);
 #define CPU_write_PC(x) do{CPU_var_reg.r[15] = x; }while(0);
+#define CPU_write_apsr_n(x) do{uint32 temp = (x & 0x1) << 31; CPU_var_reg.apsr |= temp; }while(0);
+#define CPU_write_apsr_z(x) do{uint32 temp = (x & 0x1) << 30; CPU_var_reg.apsr |= temp; }while(0);
+#define CPU_write_apsr_c(x) do{uint32 temp = (x & 0x1) << 29; CPU_var_reg.apsr |= temp; }while(0);
+#define CPU_write_apsr_v(x) do{uint32 temp = (x & 0x1) << 28; CPU_var_reg.apsr |= temp; }while(0);
 
 // functions
 
