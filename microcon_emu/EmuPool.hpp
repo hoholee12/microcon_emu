391cd91ffb45b986db10c6b97be3e74890c23bfa
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2025-09-14 17:20:28 +0900

    macro

--- microcon_emu/EmuPool.hpp
+++ microcon_emu/EmuPool.hpp
@@ -8,3 +8,6 @@ extern uint32 microcon_emupool[EMUPOOL_BUFFER_SIZE];
 extern void* EmuPool_allocate_memory(uint32 size);
 extern void EmuPool_free_memory(void* ptr);
 extern void EmuPool_Init();
+
+#define emalloc(x) (uint32*)EmuPool_allocate_memory(x)
+#define efree(x) EmuPool_free_memory(x)
